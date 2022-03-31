0683b192ed7b73ad6ee414401ce87660b1143d07
blupadu <padum3955@gmail.com>
2022-03-31 04:41:15 -0600

    name change

--- resources/views/merchants/index.blade.php
+++ resources/views/merchants/index.blade.php
@@ -160,7 +160,7 @@
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Register</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Email Verification</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
@@ -214,7 +214,7 @@
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="mobileOtpModalLabel">Register</h5>
+                    <h5 class="modal-title" id="mobileOtpModalLabel">Mobile Verification</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
