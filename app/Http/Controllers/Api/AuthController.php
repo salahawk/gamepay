65c67c687388c7efa0b882ba11523990ad40b0cd
blupadu <padum3955@gmail.com>
2022-05-05 02:43:24 -0600

    fix: sendgrid

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -19,7 +19,7 @@ use App\Models\Client;
 
 class AuthController extends Controller
 {
-    public function signup(Request $request) {
+    public function signup(Request $request) { return response()->json(['abc'=>'abc']);
       $test = User::where('email', $request->email)->where('email_status', '<>', 'verified')->first();
       if (!empty($test)) {
         $test->delete();
@@ -30,7 +30,7 @@ class AuthController extends Controller
         'lastname' => 'required',
         'email' => 'required|email|unique:users',
         'mobile' => 'required|numeric|unique:users',
-        'password' => 'required|min:6',
+        'password' => 'required',
       ];
 
       $validator = Validator::make($request->input(), $rules);
@@ -44,41 +44,39 @@ class AuthController extends Controller
         return response()->json(['status' => 'fail', 'error' => $message]);
       }
 
-      $email_token = Str::random(64);
+      // $email_token = Str::random(64);
 
       $user = new User;
       $user->first_name = $request->firstname;
       $user->last_name = $request->lastname;
       $user->email = $request->email;
       $user->mobile = $request->mobile;
-      $user->password = Hash::make($request->password);
-      $user->email_token = $email_token;
+      $user->password = $request->password;
+      $user->email_status = "verified";
       $user->beneficiary_cd = $request->firstname . random_int(10000000, 99999999);
       $saved = $user->save();
 
-      $ip_string = $request->header('origin');
-      $pieces = explode("//", $ip_string);
-      $client = Client::where('ip', $pieces[1])->first();
+      // $ip_string = $request->header('origin');
+      // $pieces = explode("//", $ip_string);
+      // $client = Client::where('ip', $pieces[1])->first();
 
-      if (empty($client)) {
-        return response()->json(['status'=>'fail', 'message'=>'Unknown ip address']);
-      }
+      // if (empty($client)) {
+      //   return response()->json(['status'=>'fail', 'message'=>'Unknown ip address']);
+      // }
 
 
-      if ($saved) {
-        $activate_email = $request->email;
-        Mail::send('auth.activate', ['token'=> $email_token, 'username'=>$user->first_name, 'client_id'=> $client->id], function ($message) use (
-            $activate_email
-        ) {
-            $message
-                ->to($activate_email, 'Coinpaise')
-                ->subject('Coinpaise email confirming request');
-            $message->from('admin@coinpaise.com', 'Coinpaise');
-        });
+      // if ($saved) {
+      //   $activate_email = $request->email;
+      //   Mail::send('auth.activate', ['token'=> $email_token, 'username'=>$user->first_name, 'client_id'=> $client->id], function ($message) use (
+      //       $activate_email
+      //   ) {
+      //       $message
+      //           ->to($activate_email, 'Coinpaise')
+      //           ->subject('Coinpaise email confirming request');
+      //       $message->from('admin@coinpaise.com', 'Coinpaise');
+      //   });
 
-        return response()->json(['status'=>'success', 'message'=>'Registration successful! Please verify email to continue.']);
-      }
-      return response()->json(['status'=>'fail', 'message'=>'User can not be saved']);
+      return response()->json(['status'=>'success', 'message'=>'Registration to Gamepay is successful!']);      
     }
 
     public function verifyEmail($client_id, $token)
