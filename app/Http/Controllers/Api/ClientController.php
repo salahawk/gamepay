2729a1dd28358950e6b8a2cbe115e46251f894b9
bluepadu <padum3955@gmail.com>
2022-05-10 03:03:45 -0700

    fix: hash generation

--- app/Http/Controllers/Api/ClientController.php
+++ app/Http/Controllers/Api/ClientController.php
@@ -351,7 +351,7 @@ class ClientController extends Controller
       }
       
       // hash generation check
-      $hash_string = $request->KEY . "|". $request->ORDER_ID . "|" . $request->AMOUNT . "|" . $request->FIRST_NAME . "|" . $request->CUST_EMAIL . "|" . $request->STATUS . "|";
+      $hash_string = "|" . $request->KEY . "|". $request->ORDER_ID . "|" . $request->AMOUNT . "|" . $request->FIRST_NAME . "|" . $request->CUST_EMAIL . "|" . $request->STATUS . "|";
       $hash_string .= env('PSP_SALT');
       $hash = hash("sha512", $hash_string);
 
