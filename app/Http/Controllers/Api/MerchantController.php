923df13ea9d12d13afa80c730f4048308cbb7e55
bluepadu <padum3955@gmail.com>
2022-05-21 02:51:48 -0700

    add: job

--- app/Http/Controllers/Api/MerchantController.php
+++ app/Http/Controllers/Api/MerchantController.php
@@ -183,7 +183,7 @@ class MerchantController extends Controller
       $url = 'https://coinsplashgifts.com/pgway/acquirernew/upipay.php'; // have to modify later based on routing logic
       $encData = urlencode(base64_encode("key=$psp_key&firstname=$customer_name&mobile=$phone&amount=$amount&email=$email&txnid=$deposit->order_id&eurl=$eurl"));
 print_r("NONONONONO");
-      ProcessStatus::dispatch($deposit->id, $url)->delay(now()->addMinutes(10));
+      ProcessStatus::dispatch($deposit->id, $url);
       return redirect()->away($url . "?encdata=" . $encData);
     }
 
