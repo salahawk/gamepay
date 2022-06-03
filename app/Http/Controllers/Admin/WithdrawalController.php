5e6f68c4029ca47cecc3cbfcbee47c0a6da5d5c8
bluepadu <padum3955@gmail.com>
2022-06-03 05:23:26 -0700

    fix: small

--- app/Http/Controllers/Admin/WithdrawalController.php
+++ app/Http/Controllers/Admin/WithdrawalController.php
@@ -30,10 +30,10 @@ class WithdrawalController extends Controller
     $order_id_sign = $request->order_id == "" ?  "like" : "=";
 
     $payouts = Payout::where('created_at', '>=', $from . " 00:00:00")
-      ->where('created_at', '<=', $to . " 23:59:59")
-      ->where('status', $status_sign, $status)
-      ->where('email', $email_sign, $email)
-      ->where('order_id', $order_id_sign, $order_id)
+      // ->where('created_at', '<=', $to . " 23:59:59")
+      // ->where('status', $status_sign, $status)
+      // ->where('email', $email_sign, $email)
+      // ->where('order_id', $order_id_sign, $order_id)
       ->orderby('created_at', 'desc')->select('*');
 
     return DataTables::of($payouts)
