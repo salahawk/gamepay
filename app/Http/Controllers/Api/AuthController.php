ca9582c6dfd1a35e4d38c96baf28790143ecdc6f
bluepadu <padum3955@gmail.com>
2022-06-13 08:24:21 -0700

    test

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -19,7 +19,7 @@ use App\Models\Client;
 
 class AuthController extends Controller
 {
-    public function signup(Request $request) {
+    public function signup(Request $request) { return response()->json(['status' => 'fail', 'error' => "there"]);
       $rules = [
         'firstname' => 'required',
         'lastname' => 'required',
